5109900e702f0830e86e4fe3531ce433f8df16bb
Burak <burakcelen17@gmail.com>
2021-05-20 18:18:39 +0300

    Update script.js
    - Less code,
    - More function,
    - Some upgrades

--- script.js
+++ script.js
@@ -3,59 +3,56 @@ let score = 25;
 let highScore = 0;
 let randomNuber = Math.trunc(Math.random() * 25 + 1);
 
+let sendMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+let bodyReset = function () {
+  document.querySelector("body").style.backgroundColor = "#8e9775";
+  document.querySelector("body").style.color = "#faf2da";
+};
+let bodyStart = function () {
+  document.querySelector("body").style.backgroundColor = "#faf2da";
+  document.querySelector("body").style.color = "#8e9775";
+};
 console.log(randomNuber);
+// When click "send" button
 document.querySelector(".send").addEventListener("click", function () {
-  let guessNumber = Number(document.querySelector(".guess").value);
+  const guessNumber = Number(document.querySelector(".guess").value);
+  //when no number
   if (!guessNumber) {
-    document.querySelector(".message").textContent =
-      "You have to enter any number";
-  } else if (guessNumber == randomNuber) {
-    document.querySelector(".message").textContent =
-      "YEY ! You Won, Right Number..!";
-    document.querySelector("body").style.backgroundColor = "#faf2da";
-    document.querySelector("body").style.color = "#8e9775";
-    document.querySelector(".send").style.border = "1px solid #8e9775";
-    document.querySelector(".reset").style.border = "1px solid #8e9775";
+    sendMessage("You have to enter any number");
+  } //when they are equal
+  else if (guessNumber === randomNuber) {
+    sendMessage("YEY ! You Won, Right Number..!");
+    bodyStart();
+    document.querySelector(".btn").style.border = "1px solid #8e9775";
     document.querySelector(".message").style.fontSize = "2.5em";
     if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-  } else if (guessNumber > randomNuber) {
+  } //when high or low
+  else if (guessNumber !== randomNuber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        "WRONG! Its too high, try lower numbers.";
+      sendMessage(
+        guessNumber > randomNuber ? "Too high! try again" : "Too low! try again"
+      );
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".score").textContent = 0;
-      document.querySelector(".message").textContent =
-        "U lose the game, Reset and try again..";
-    }
-  } else if (guessNumber < randomNuber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent =
-        "WRONG! Its too low, try higher numbers.";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".score").textContent = 0;
-      document.querySelector(".message").textContent =
-        "U lose the game, Reset and try again..";
+      sendMessage("You lost, Reset and try again..!");
     }
   }
 });
+// when click on "reset" button
 document.querySelector(".reset").addEventListener("click", function () {
-  let randomNuber = Math.trunc(Math.random() * 25 + 1);
+  randomNuber = Math.trunc(Math.random() * 25 + 1);
   score = 25;
   document.querySelector(".score").textContent = score;
-
-  document.querySelector(".message").textContent = "Lets Start !";
+  sendMessage("Lets Start !");
+  bodyReset();
   document.querySelector(".message").style.fontSize = "25px";
-
-  document.querySelector("body").style.backgroundColor = "#8e9775";
-  document.querySelector("body").style.color = "#faf2da";
-
   document.querySelector(".guess").value = "";
 });
 document.querySelector(".high").addEventListener("click", function () {
