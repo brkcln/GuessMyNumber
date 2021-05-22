0c369460ff0a7af0e892386c445387b715a5dd1a
Burak <burakcelen17@gmail.com>
2021-05-22 22:19:38 +0300

    Update script.js
    -More function, less code,
    -advanced guess text added,
    - It shows the number when the game is over added,
    -its 1 and 50 now,
    -timer layout added (not working).

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
-let score = 25;
+let score = 10;
 let highScore = 0;
-let randomNuber = Math.trunc(Math.random() * 25 + 1);
+let randomNuber = Math.trunc(Math.random() * 50 + 1);
+let a = randomNuber - 5;
+let b = randomNuber + 5;
 
 let sendMessage = function (message) {
   document.querySelector(".message").textContent = message;
@@ -16,6 +18,7 @@ let bodyStart = function () {
 };
 console.log(randomNuber);
 // When click "send" button
+
 document.querySelector(".send").addEventListener("click", function () {
   const guessNumber = Number(document.querySelector(".guess").value);
   //when no number
@@ -23,6 +26,7 @@ document.querySelector(".send").addEventListener("click", function () {
     sendMessage("You have to enter any number");
   } //when they are equal
   else if (guessNumber === randomNuber) {
+    document.querySelector(".timerLeft").style.visibility = "hidden";
     sendMessage("YEY ! You Won, Right Number..!");
     bodyStart();
     document.querySelector(".btn").style.border = "1px solid #8e9775";
@@ -31,30 +35,59 @@ document.querySelector(".send").addEventListener("click", function () {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
+    timerReset();
   } //when high or low
   else if (guessNumber !== randomNuber) {
     if (score > 1) {
-      sendMessage(
-        guessNumber > randomNuber ? "Too high! try again" : "Too low! try again"
-      );
+      if (guessNumber > randomNuber) {
+        if (guessNumber <= b) {
+          // when guessnumber "random + 5 > guess number > random"
+          sendMessage("Too close, try lower numbers !");
+        } else {
+          sendMessage("Try lower numbers !");
+        }
+      } else {
+        if (guessNumber >= a) {
+          // when guess number "random - 5 < guess number < random"
+          sendMessage("Too close, try higher numbers !");
+        } else {
+          sendMessage("Try higher numbers !");
+        }
+      }
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".score").textContent = 0;
-      sendMessage("You lost, Reset and try again..!");
+      sendMessage(`Correct number was ${guessNumber} !`);
     }
   }
 });
 // when click on "reset" button
-document.querySelector(".reset").addEventListener("click", function () {
-  randomNuber = Math.trunc(Math.random() * 25 + 1);
-  score = 25;
+document.querySelector(".reset").addEventListener("click", resetAll);
+//when click on "reset highscore" button
+document.querySelector(".high").addEventListener("click", function () {
+  document.querySelector(".highscore").textContent = "0";
+});
+
+// let isRunning = true;
+// function controlTime() {
+//   if (isRunning) {
+//     clearInterval(timer);
+//     isRunning = false;
+//   } else {
+//     timer = setInterval(interVal, 1000);
+//     isRunning = true;
+//     interVal();
+//   }
+// }
+function resetAll() {
+  randomNuber = Math.trunc(Math.random() * 50 + 1);
+  score = 10;
   document.querySelector(".score").textContent = score;
   sendMessage("Lets Start !");
   bodyReset();
   document.querySelector(".message").style.fontSize = "25px";
   document.querySelector(".guess").value = "";
-});
-document.querySelector(".high").addEventListener("click", function () {
-  document.querySelector(".highscore").textContent = "0";
-});
+  console.log(randomNuber);
+  timerReset();
+}
